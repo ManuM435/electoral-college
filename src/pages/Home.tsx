c9b7614e170001f16c31c86425372e45bae81f10
DragonWarrior💫 <supremedragonwarrior@gmail.com>
2025-01-14 18:50:54 -0300

    base for map

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,8 @@ import React from 'react';
 const Home: React.FC = () => {
   return (
     <div className="container mx-auto p-4 mt-16">
-      <h1 className="text-2xl font-bold mb-4">Welcome to the Electoral College Estimator</h1>
+      <h1 className="text-2xl font-bold mb-4">Select the Countries to be added as States!</h1>
+      <h3 className="text-lg font-semibold mb-4">And see how the Electoral College would look</h3>
       <p>aca va el mapita</p>
     </div>
   );
